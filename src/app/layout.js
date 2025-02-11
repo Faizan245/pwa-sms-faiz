0322bec1b6df29593abaf11f2a171c968309094d
Faizan Ansari <hussainfaizan1379@gmail.com>
2025-02-11 21:43:59 +0530

    ok

--- src/app/layout.js
+++ src/app/layout.js
@@ -58,7 +58,7 @@ export default function RootLayout({ children }) {
       </html>
     );
   }
-
+//hello
   return (
     <html lang="en">
       <head>
--- start.bat
+++ start.bat
@@ -4,4 +4,4 @@ cd /d %~dp0
 echo Starting Next.js server...
 set PORT=4000
 "C:\Program Files\nodejs\node.exe" .next\standalone\server.js
-pause
+exit
